99081df2e6eb3a74706af98ae706ded1f312001f
kihyeoon <bangion93@gmail.com>
2022-08-23 00:36:28 +0900

    3.6 Themes

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,16 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-// const darkTheme = {
-//   textColor: "whitesmoke",
-//   backgroundColor: "#111",
-// };
-// const lightTheme = {
-//   textColor: "#111",
-//   backgroundColor: "whitesmoke",
-// };
+import { darkTheme, lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
