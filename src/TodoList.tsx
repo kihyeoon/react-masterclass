b74fab3e4b59e93f670f7d1b3efe40b89e8dc547
kihyeoon <bangion93@gmail.com>
2022-11-07 23:46:45 +0900

    5.9 Custom Validation

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,25 +1,35 @@
 import { useForm } from "react-hook-form";
 
-type IForm = {
-  errors: string;
+interface IForm {
   username: string;
   email: string;
   password: string;
   password1: string;
-};
+  extraErrors?: string;
+}
 
 function TodoList() {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => console.log(data);
-  console.log(errors);
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      return setError(
+        "password1",
+        { message: "비밀번호가 일치하지 않습니다." },
+        { shouldFocus: true }
+      );
+    }
+    setError("extraErrors", { message: "Server offline." });
+  };
+  console.log("err", errors);
 
   return (
     <div>
@@ -33,7 +43,7 @@ function TodoList() {
             required: "email is required",
             minLength: 5,
             pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
               message: "네이버 이메일만 가능합니다.",
             },
           })}
@@ -42,7 +52,17 @@ function TodoList() {
         <span>{errors?.email?.message}</span>
 
         <input
-          {...register("username", { required: "작성해주세요.", minLength: 5 })}
+          style={{ borderColor: errors?.email?.message ? "red" : "" }}
+          {...register("username", {
+            required: "작성해주세요.",
+            minLength: 3,
+            validate: {
+              noNico: (value) =>
+                value.includes("nico") ? "nico는 포함할 수 없습니다." : true,
+              noNick: (value) =>
+                value.includes("nick") ? "nick은 포함할 수 없습니다." : true,
+            },
+          })}
           placeholder="Username"
         />
         <span>{errors?.username?.message}</span>
@@ -69,6 +89,7 @@ function TodoList() {
         <span>{errors?.password1?.message}</span>
 
         <button>Add</button>
+        <span>{errors?.extraErrors?.message}</span>
       </form>
     </div>
   );
