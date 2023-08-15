469f6cf5980ee604958b2728d581b831f0e1149b
kihyeoon <bangion93@gmail.com>
2023-08-15 18:48:06 +0900

    6.0 Get Selectors

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,46 +1,14 @@
 import { atom, selector } from "recoil";
 
-export enum Category {
-  "TODO" = "TODO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Category;
-}
-
-export const categoryState = atom<Category>({
-  key: "category",
-  default: Category.TODO,
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-  effects: [
-    ({ setSelf, onSet }) => {
-      const TODO_KEY = "Todo";
-      const savedValue = localStorage.getItem(TODO_KEY);
-      if (savedValue !== null) {
-        setSelf(JSON.parse(savedValue));
-      }
-      onSet((newValue, _, isReset) => {
-        isReset
-          ? localStorage.removeItem(TODO_KEY)
-          : localStorage.setItem(TODO_KEY, JSON.stringify(newValue));
-      });
-    },
-  ],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+    const minutes = get(minuteState);
+    return Math.floor(minutes / 60);
   },
 });
