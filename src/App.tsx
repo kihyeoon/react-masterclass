a72ed0189ada8776bc5842b07f7aa41e577210f9
kihyeoon <bangion93@gmail.com>
2023-08-27 15:51:44 +0900

    7.7 MotionValues part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { Variants, motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -20,48 +20,15 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const ContainerBox = styled(motion.div)`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-`;
-
-const boxVariants: Variants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: "100px" },
-  drag: {
-    backgroundColor: "rgb(98, 229, 153)",
-  },
-};
-
 function App() {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  useEffect(() => {
+    x.onChange(() => console.log(x.get()));
+  }, [x]);
   return (
     <Wrapper>
-      <ContainerBox ref={containerRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          variants={boxVariants}
-          whileHover="hover"
-          whileDrag="drag"
-          whileTap="click"
-        ></Box>
-        <Box
-          drag
-          dragElastic={0}
-          dragConstraints={containerRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileDrag="drag"
-          whileTap="click"
-        ></Box>
-      </ContainerBox>
+      <button onClick={() => x.set(200)}>Reset</button>
+      <Box drag="x" dragSnapToOrigin style={{ x }} />
     </Wrapper>
   );
 }
