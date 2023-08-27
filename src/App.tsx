7cc6a587f2064934acb34e95326cb50f211c446d
kihyeoon <bangion93@gmail.com>
2023-08-27 11:29:18 +0900

    7.4 Variants part Two - y축 이동이 opacity 애니메이션보다 먼저 작동하는 현상으로 인해 5.0.0 버전으로 재설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { Variants, motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -12,31 +12,61 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVariants = {
+const Circle = styled(motion.div)`
+  background-color: white;
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants: Variants = {
   start: {
+    opacity: 0,
     scale: 0,
   },
   end: {
-    rotate: 180,
+    opacity: 1,
     scale: 1,
     transition: {
       type: "spring",
-      stiffness: 260,
-      damping: 10,
-      delay: 0.5,
+      duration: 0.5,
+      bounce: 0.5,
+      delay: 1,
+      delayChildren: 1.5,
+      staggerChildren: 0.2,
     },
+  },
+};
+
+const circleVariants: Variants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVariants} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
