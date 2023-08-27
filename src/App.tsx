61ce637bb2447100e851ddee172b8a76612d49a6
kihyeoon <bangion93@gmail.com>
2023-08-27 14:11:53 +0900

    7.6 Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Variants, motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -15,29 +16,52 @@ const Box = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 25px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const ContainerBox = styled(motion.div)`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const boxVariants: Variants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
+  hover: { rotateZ: 90 },
+  click: { borderRadius: "100px" },
   drag: {
-    backgroundColor: "rgba(46, 204, 113, 1)",
-    transition: { duration: 3 },
+    backgroundColor: "rgb(98, 229, 153)",
   },
 };
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag="drag"
-        whileTap="click"
-      ></Box>
+      <ContainerBox ref={containerRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="click"
+        ></Box>
+        <Box
+          drag
+          dragElastic={0}
+          dragConstraints={containerRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="click"
+        ></Box>
+      </ContainerBox>
     </Wrapper>
   );
 }
