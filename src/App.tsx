515103cbb8f162751b7fe8c3c06d45237d024f3b
kihyeoon <bangion93@gmail.com>
2023-09-10 13:00:20 +0900

    8.1 Header part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return null;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
+  );
+}
