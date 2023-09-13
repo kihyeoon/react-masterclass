b272e0cc4e59c7c6ffd1fd4d800f22f5d90ff78d
kihyeoon <bangion93@gmail.com>
2023-09-13 23:21:27 +0900

    8.6 Home Screen part Two

--- src/index.tsx
+++ src/index.tsx
@@ -60,7 +60,8 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
-    color:black;
+    color:${(props) => props.theme.white.darker};
+    background-color: black;
     line-height: 1.2;
   }
   a {
